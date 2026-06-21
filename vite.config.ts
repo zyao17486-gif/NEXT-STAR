6229eb7b7de4d2c8934b3492847cbd5c8e1894e8
Next Star <next-star@example.com>
2026-06-21 22:40:27 +0800

    Fix: base path uppercase NEXT-STAR

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
-  base: '/next-star/',
+  base: '/NEXT-STAR/',
   plugins: [
     figmaAssetResolver(),
     // The React and Tailwind plugins are both required for Make, even if
